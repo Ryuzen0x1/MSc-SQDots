59b62e716f32900cbd53d3632377c7c2d680f718
Shaon <ryuki@windows-vm.com>
2025-02-08 15:10:21 +0530

    some improvements

--- chi_1.m
+++ chi_1.m
@@ -1,29 +1,43 @@
 clear;
 close all;
-deltap = -5.0e11 : 0.01e11 : 5.0e11; 
-deltac1=1.0e9;
-deltac2=2.0e9;
+ax = gca;
+
+% Parameters
+deltap = -5.0e11:0.01e11:5.0e11; 
+deltac=0;
+deltab=0;
 g21 = 1.0e9;  
 g31 = 1.0e9;   
-g41 = 4.2e11;
-Omega_c1 = 1.5e11;
-Omega_c2 = 1.0e11;
-K = 1.4e17;
-omegap = 10.08e14;
-c = 3e8;
+g41 = 4.0e11;
+omegac = 1.5e11;
+omegab = 1.0e11;
+K = 2.0e17;
+omegap = 11.0e14;
+c = 3e8; % Speed of light
 
+% Complex detuning terms
 Delta1 = deltap+1i*(g21/2);
-Delta2 = deltap +deltac1+1i*(g31/2);
-Delta3 = deltap +deltac2+1i*(g41/2);
+Delta2 = deltap +deltac+1i*(g31/2);
+Delta3 = deltap +deltab+1i*(g41/2);
 
-Dp = (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2) - abs(Omega_c2)^2;
-D = (Delta1 + 1i*g21/2) .* (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2)- abs(Omega_c1)^2 .* (Delta3 + 1i*g41/2)- abs(Omega_c2)^2 .* (Delta1 + 1i*g21/2);
+% Numerator and Detuning terms
+Dp = (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2) - abs(omegab)^2;
+D = (Delta1 + 1i*g21/2) .* (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2)- abs(omegac)^2 .* (Delta3 + 1i*g41/2)- abs(omegab)^2 .* (Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
 l=(A^2)*84000;
 
 chi1=((-A.*Dp)./D);
 
-plot(deltap ./ 10^11,imag(chi1),'b', 'LineWidth', 1.5);
+% Plot
+red = [0.98 0.52 0];
+blue = [0.12 0.61 0.73];
+plot(deltap ./ 10^11,imag(chi1), 'red', 'LineWidth', 1.5);
 hold on;
-plot(deltap ./ 10^11,real(chi1),'r', 'LineWidth', 1.5);
+plot(deltap ./ 10^11,real(chi1), 'blue', 'LineWidth', 1.5);
+grid on;
+
+ax.XAxis.FontSize = 12;
+ax.YAxis.FontSize = 12;
+xlabel('Probe Freq. (\Delta_p)', 'FontSize', 12, 'FontWeight', 'bold');
+ylabel('First order susceptibility (\chi^{(1)})', 'FontSize', 12, 'FontWeight', 'bold');
 ylim([-3,5]);
--- chi_3.m
+++ chi_3.m
@@ -1,45 +1,47 @@
 clear;
 close all;
-ax=gca;
+ax = gca;
 
 % Parameters
-mu41=15.68e-29;
-hbar=1.05e-34;
-deltap=-5e13:0.001e13:5e13;
-deltac=0;
-deltad=0;
-deltab=0;
-phi=0;
-omegac=1.0e11;
-omegap=2.0e12;
-omegad=0e12;
-omegab=0e12;
-g21=1.51e11;
-g31=3.03e12;
-g41=3.03e12;
-k=1.24e15;
-c=3e8; % Speed of light
+mu41 = 15.68e-29;
+hbar = 1.05e-34;
+deltap = -5e13:0.001e13:5e13;
+deltac = 0;
+deltad = 0;
+deltab = 0;
+phi = 0;
+omegac = 1.0e11;
+omegap = 2.0e12;
+omegad = 0e12;
+omegab = 0e12;
+g21 = 1.51e11;
+g31 = 3.03e12;
+g41 = 3.03e12;
+k = 1.24e15;
+c = 3e8; % Speed of light
 
 % Complex detuning terms
-X=(deltap-deltab+(1i/2).*g21);
-Y=(deltap-deltad+(1i/2).*g31);
-Z=(deltap+(1i/2).*g41);
+X = (deltap-deltab+(1i/2).*g21);
+Y = (deltap-deltad+(1i/2).*g31);
+Z = (deltap+(1i/2).*g41);
 
 % Numerator and Denominator terms
-Dp=(X.*Z-abs(omegac).^2);
-D=(X.*Y.*Z-Z.*abs(omegac).^2-X.*abs(omegad).^2-Y.*abs(omegab).^2+2*(omegac)*(omegad)*(omegab)*cos(phi));
-D2=(Y.*abs(omegab)-abs(omegac*omegad)*exp(1i*phi));
-D3=(X.*abs(omegad)-abs(omegac*omegab)*exp(-1i*phi));
-A=((2.*c.*k)./(omegap));
-B=((abs(Dp).^2+abs(D2).^2+abs(D3).^2)/abs(D).^2);
+Dp = (X.*Z-abs(omegac).^2);
+D = (X.*Y.*Z-Z.*abs(omegac).^2-X.*abs(omegad).^2-Y.*abs(omegab).^2+2*(omegac)*(omegad)*(omegab)*cos(phi));
+D2 = (Y.*abs(omegab)-abs(omegac*omegad)*exp(1i*phi));
+D3 = (X.*abs(omegad)-abs(omegac*omegab)*exp(-1i*phi));
+A = ((2.*c.*k)./(omegap));
+B = ((abs(Dp).^2+abs(D2).^2+abs(D3).^2)/abs(D).^2);
 
 %chi1=((-A.*Dp)./D);
-chi3=((A.*Dp)./D)*(abs(mu41).^2/((hbar).^2))*B;
+chi3 = ((A.*Dp)./D)*(abs(mu41).^2/((hbar).^2))*B;
 
 % Plot
-plot(deltap./10.^13,real(chi3), 'Color', [0 0 1], 'LineWidth', 1.5);
+red = [0.98 0.52 0];
+blue = [0.12 0.61 0.73];
+plot(deltap./10.^13,real(chi3), 'blue', 'LineWidth', 1.5);
 hold on;
-plot(deltap./10.^13,imag(chi3), 'Color', [1 0 0], 'LineWidth', 1.5);
+plot(deltap./10.^13,imag(chi3), 'red', 'LineWidth', 1.5);
 
 ax.XAxis.FontSize = 12;
 ax.YAxis.FontSize = 12;
