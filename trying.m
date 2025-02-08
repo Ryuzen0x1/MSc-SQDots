5a0c9188c3200d1972226171bd2054e09b08acd6
Shaon <ryuki@windows-vm.com>
2025-02-08 17:36:08 +0530

    fix and add

--- trying.m
+++ trying.m
@@ -7,31 +7,29 @@ deltac = 1e12;
 deltad = 2.0e12;
 % omega = 0;
 K = 1.4e17;
-Omega_b = 0.5e12;
-Omega_c = 1e12;
-Omega_p = 10.08e14;
-gamma_21 = 2.31e11;                 % (\gamma_{21})
-gamma_31 = 2.10e9;                 % (\gamma_{31})
-gamma_41 = 2.21e11;                 % (\gamma_{41})
-gamma_2 = 1.2e11;
-gamma_4 = 2.5e12;
+omegab = 0.5e12;
+omegac = 1e12;
+omegap = 10.08e14;
+g21 = 2.31e11;                 % (\gamma_{21})
+g31 = 2.10e9;                 % (\gamma_{31})
+g41 = 2.21e11;                 % (\gamma_{41})
 c = 3e8;
 
 % Complex detuning terms
-d21 = deltap + 1i * (gamma_21 / 2);
-d31 = deltap + deltac + 1i * (gamma_31 / 2);
-d41 = deltap + deltad + 1i * (gamma_41 / 2);
+d21 = deltap + 1i * (g21 / 2);
+d31 = deltap + deltac + 1i * (g31 / 2);
+d41 = deltap + deltad + 1i * (g41 / 2);
 
 % Numerator and denominator terms
-Dp = (deltap + 1i * gamma_2/2) .* (deltap + deltad + 1i * gamma_4/2) - abs(Omega_b)^2;
+Dp = (deltap) .* (deltap + deltad) - abs(omegab)^2;
 
-D = (deltap + 1i * gamma_2/2) .* (deltap + deltad + 1i * gamma_4/2) .* ...
-    (deltap + deltac + 1i * gamma_31/2) - ...
-    abs(Omega_b)^2 .* (deltap + deltac + 1i * gamma_31/2) - ...
-    abs(Omega_c)^2 .* (deltap + deltad + 1i * gamma_4/2);
+D = (deltap) .* (deltap + deltad) .* ...
+    (deltap + deltac + 1i * g31/2) - ...
+    abs(omegab)^2 .* (deltap + deltac + 1i * g31/2) - ...
+    abs(omegac)^2 .* (deltap + deltad);
 
 % Third-order susceptibility (\chi_3)
-A = (2 .* K .* c) ./ Omega_p; % Normalization constant
+A = (2 .* K .* c) ./ omegap; % Normalization constant
 chi1 = ((-A.*Dp) ./ D);
 %chi3 = abs(A).^2 .* Dp ./ (D.^2 .* d21);
 
