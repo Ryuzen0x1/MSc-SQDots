37eef114b050fd1cf3c1b5939be4ea594e6baca2
MoonRyu <57990129+Ryuzen0x1@users.noreply.github.com>
2025-04-15 22:13:13 +0530

    Update chi_3_Omega_d.m

--- Assets/chi_3_Omega_d.m
+++ Assets/chi_3_Omega_d.m
@@ -1,86 +1,104 @@
 clear;
 close all;
 
-deltap=-2*10^12:0.001*10^12:2*10^12;
-deltac=1.0*10^9;
-deltab=2.0*10^9;
-omega_c=5.0*10^11;
-g21=2.0*10^9;
-g31=1.0*10^9;
-g41=4.2*10^11;
-K=1.4*10^17;
-omegap=10.08*10^14;
-omega_d=2*10^11;
-c=3*10^8;
+deltap=-2e12:0.001e12:2e12;
+deltac=1.0e9;
+deltab=2.0e9;
+omega_c=5.0e11;
+g21=2.0e9;
+g31=1.0e9;
+g41=4.2e11;
+K=1.4e17;
+omegap=10.08e14;
+omega_d=3e11;
+c=3e8;
 
 Delta1 = deltap+1i*(g21/2);
 Delta2 = deltap +deltac+1i*(g31/2);
 Delta3 = deltap +deltab+1i*(g41/2);
 
 Dp = (Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_d)^2;
-D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_c)^2.*(Delta3 + 1i*g41/2) - abs(omega_d)^2.*(Delta1 + 1i*g21/2);
+D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - ... 
+    abs(omega_c)^2.*(Delta3 + 1i*g41/2) - abs(omega_d)^2.*(Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
-l=3.90*10^12;
+l=3.90e12;
 chi1=((-A.*Dp)./D);
 X=(abs(omega_c).^2)*(abs(Delta3 + 1i*g41/2).^2);
 Y=abs(omega_d)*abs(omega_c);
 chi3 =- l*chi1*((abs(Dp).^2 + X+ Y.^2)/ abs(D).^2);
-plot(deltap./10.^11, real(chi3));
+plot(deltap./10.^11, real(chi3),'-.' , LineWidth=2, DisplayName='\Omega_d=2.0'); % Blue
 hold on;
 
-omega_c=5.0*10^11;
-omega_d=4*10^11;
+omega_c=5.0e11;
+omega_d=5e11;
 Dp = (Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_d)^2;
-D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2)- abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
+D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - ...
+    abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
-l=3.90*10^12;
+l=3.90e12;
 chi1=((-A.*Dp)./D);
 X=(abs(omega_c).^2)*(abs(Delta3 + 1i*g41/2).^2);
 Y=abs(omega_d)*abs(omega_c);
 chi3 =- l*chi1*((abs(Dp).^2 + X + Y.^2)/ abs(D).^2);
-plot(deltap./10.^11, real(chi3));
+plot(deltap./10.^11, real(chi3), LineWidth=2, DisplayName='\Omega_d=4.0'); % Orange
 hold on;
 
 
-omega_c=5.0*10^11;
-omega_d=6*10^11;
+omega_c=5.0e11;
+omega_d=7e11;
 Dp = (Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_d)^2;
-D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2)- abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
+D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - ...
+    abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
-l=3.90*10^12;
+l=3.90e12;
 chi1=((-A.*Dp)./D);
 X=(abs(omega_c).^2)*(abs(Delta3 + 1i*g41/2).^2);
 Y=abs(omega_d)*abs(omega_c);
 chi3 =- l*chi1*((abs(Dp).^2 + X + Y.^2)/ abs(D).^2);
-plot(deltap./10.^11, real(chi3));
+plot(deltap./10.^11, real(chi3), LineWidth=2, DisplayName='\Omega_d=6.0'); % Yellow
 hold on;
 
-omega_c=5.0*10^11;
-omega_d=8*10^11;
+omega_c=5.0e11;
+omega_d=9e11;
 Dp = (Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_d)^2;
-D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2)- abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
+D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - ...
+    abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
-l=3.90*10^12;
+l=3.90e12;
 chi1=((-A.*Dp)./D);
 X=(abs(omega_c).^2)*(abs(Delta3 + 1i*g41/2).^2);
 Y=abs(omega_d)*abs(omega_c);
 chi3 =- l*chi1*((abs(Dp).^2 + X+ Y.^2)/ abs(D).^2);
-plot(deltap./10.^11, real(chi3));
+plot(deltap./10.^11, real(chi3), '--', LineWidth=2.5, DisplayName='\Omega_d=8.0'); % Purple
 hold on;
 
-omega_c=5.0*10^11;
-omega_d=10*10^11;
+omega_c=5.0e11;
+omega_d=11e11;
 Dp = (Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_d)^2;
-D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2)- abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
+D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - ...
+    abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
-l=3.90*10^12;
+l=3.90e12;
 chi1=((-A.*Dp)./D);
 X=(abs(omega_c).^2)*(abs(Delta3 + 1i*g41/2).^2);
 Y=abs(omega_d)*abs(omega_c);
 chi3 =- l*chi1*((abs(Dp).^2 + X + Y.^2)/ abs(D).^2);
-plot(deltap./10.^11, real(chi3));
+plot(deltap./10.^11, real(chi3), LineWidth=2, DisplayName='\Omega_d=10.0'); % Green
 hold on;
-xlabel('\Delta_p');
-ylabel('Re \chi^{(3)}');
-title('\Delta_p vs \chi^{(3)}');
+
+lgd = legend('show');
+set(lgd, 'FontName', 'Garamond', 'FontSize', 16, 'FontWeight', 'bold', 'EdgeColor', 'w', 'NumColumns', 2);
+legend boxon;
+
+set(gca, FontName='Garamond', FontSize=18, FontWeight='bold');
+xlabel('\Delta_p', FontName='Garamond', FontSize=18, FontWeight='bold');
+ylabel('Re \chi^{(3)}', FontName='Garamond', FontSize=18, FontWeight='bold');
+title('\Delta_p vs Re\chi^{(3)}', FontName='Garamond', FontSize=17, FontWeight='bold');
 grid on;
+
+%folder_path = "C:\Users\Ryuki\Downloads\PLOTS";
+%file = fullfile(folder_path, 'chi3_Omega_d.png');
+%fig = gcf;
+%set(fig, 'Units', 'pixels');
+%fig.Position = [100, 100, 1920, 1080];  % [left, bottom, width, height]
+%exportgraphics(fig, file, 'Resolution', 600);
