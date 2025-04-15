f3e2ff739f3157b778d59c1b9c55525cb59e0f81
MoonRyu <57990129+Ryuzen0x1@users.noreply.github.com>
2025-04-15 21:32:50 +0530

    Update and rename chi_1_c.m to chi_3_Omega_d.m

--- Assets/chi_3_Omega_d.m
+++ Assets/chi_3_Omega_d.m
@@ -1,8 +1,9 @@
 clear;
 close all;
-deltap=-3*10^12:0.001*10^12:3*10^12;
-deltac1=1.0*10^9;
-deltac2=2.0*10^9;
+
+deltap=-2*10^12:0.001*10^12:2*10^12;
+deltac=1.0*10^9;
+deltab=2.0*10^9;
 omega_c=5.0*10^11;
 g21=2.0*10^9;
 g31=1.0*10^9;
@@ -11,11 +12,13 @@ K=1.4*10^17;
 omegap=10.08*10^14;
 omega_d=2*10^11;
 c=3*10^8;
-Delta1 = deltap; 
-Delta2 = Delta1 +deltac1;
-Delta3 = Delta2 +deltac2; 
-Dp = (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2) - abs(omega_d)^2;
-D = (Delta1 + 1i*g21/2) .* (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2)- abs(omega_c)^2 .* (Delta3 + 1i*g41/2)- abs(omega_d)^2 .* (Delta1 + 1i*g21/2);
+
+Delta1 = deltap+1i*(g21/2);
+Delta2 = deltap +deltac+1i*(g31/2);
+Delta3 = deltap +deltab+1i*(g41/2);
+
+Dp = (Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_d)^2;
+D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_c)^2.*(Delta3 + 1i*g41/2) - abs(omega_d)^2.*(Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
 l=3.90*10^12;
 chi1=((-A.*Dp)./D);
@@ -27,24 +30,22 @@ hold on;
 
 omega_c=5.0*10^11;
 omega_d=4*10^11;
-Dp = (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2) - abs(omega_d)^2;
-D = (Delta1 + 1i*g21/2) .* (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2)- abs(omega_c)^2 .* (Delta3 + 1i*g41/2)- abs(omega_d)^2 .* (Delta1 + 1i*g21/2);
+Dp = (Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_d)^2;
+D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2)- abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
 l=3.90*10^12;
 chi1=((-A.*Dp)./D);
 X=(abs(omega_c).^2)*(abs(Delta3 + 1i*g41/2).^2);
 Y=abs(omega_d)*abs(omega_c);
 chi3 =- l*chi1*((abs(Dp).^2 + X + Y.^2)/ abs(D).^2);
-
 plot(deltap./10.^11, real(chi3));
-
 hold on;
 
 
 omega_c=5.0*10^11;
 omega_d=6*10^11;
-Dp = (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2) - abs(omega_d)^2;
-D = (Delta1 + 1i*g21/2) .* (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2)- abs(omega_c)^2 .* (Delta3 + 1i*g41/2)- abs(omega_d)^2 .* (Delta1 + 1i*g21/2);
+Dp = (Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_d)^2;
+D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2)- abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
 l=3.90*10^12;
 chi1=((-A.*Dp)./D);
@@ -54,11 +55,10 @@ chi3 =- l*chi1*((abs(Dp).^2 + X + Y.^2)/ abs(D).^2);
 plot(deltap./10.^11, real(chi3));
 hold on;
 
-
 omega_c=5.0*10^11;
 omega_d=8*10^11;
-Dp = (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2) - abs(omega_d)^2;
-D = (Delta1 + 1i*g21/2) .* (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2)- abs(omega_c)^2 .* (Delta3 + 1i*g41/2)- abs(omega_d)^2 .* (Delta1 + 1i*g21/2);
+Dp = (Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_d)^2;
+D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2)- abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
 l=3.90*10^12;
 chi1=((-A.*Dp)./D);
@@ -67,10 +67,11 @@ Y=abs(omega_d)*abs(omega_c);
 chi3 =- l*chi1*((abs(Dp).^2 + X+ Y.^2)/ abs(D).^2);
 plot(deltap./10.^11, real(chi3));
 hold on;
+
 omega_c=5.0*10^11;
 omega_d=10*10^11;
-Dp = (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2) - abs(omega_d)^2;
-D = (Delta1 + 1i*g21/2) .* (Delta2 + 1i*g31/2) .* (Delta3 + 1i*g41/2)- abs(omega_c)^2 .* (Delta3 + 1i*g41/2)- abs(omega_d)^2 .* (Delta1 + 1i*g21/2);
+Dp = (Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2) - abs(omega_d)^2;
+D = (Delta1 + 1i*g21/2).*(Delta2 + 1i*g31/2).*(Delta3 + 1i*g41/2)- abs(omega_c)^2.*(Delta3 + 1i*g41/2)- abs(omega_d)^2.*(Delta1 + 1i*g21/2);
 A=((2.*c.*K)./(omegap));
 l=3.90*10^12;
 chi1=((-A.*Dp)./D);
